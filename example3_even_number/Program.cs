b30eaeb0ddcb834eb9220d5ff941e514b954553e
VassiliyZh <111jvv@mail.ru>
2022-12-18 21:06:57 +0500

    Определение четного числа

---
+++ example3_even_number/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0)
+{
+  Console.WriteLine(number + " является четным числом");  
+}
+else
+{
+    Console.WriteLine(number + " является нечетным числом");
+}
