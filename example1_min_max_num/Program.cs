9915863a7fa40abb3b3dea15482c0ab160976fac
VassiliyZh <111jvv@mail.ru>
2022-12-18 18:42:46 +0500

    Определение максимального и минимального сисла

--- example1_min_max_num/Program.cs
+++ example1_min_max_num/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Вводим два числа, на выходе увидим какой из них мин, какой макс
+Console.WriteLine("Введите число а");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+    {
+        Console.WriteLine("a является max, b является min числом");
+    }
+else
+    {
+        Console.WriteLine("a является min, b является max числом");
+    }
