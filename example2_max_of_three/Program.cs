cd271a1c81da48c045d811064f1c05f6b19dd8a9
VassiliyZh <111jvv@mail.ru>
2022-12-18 20:09:21 +0500

    Максимальное число из трех

---
+++ example2_max_of_three/Program.cs
@@ -0,0 +1,18 @@
+﻿// Вводим три числа, на выходе увидим какое из них максимальное
+Console.WriteLine("Введите число а");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число с");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (max < b)
+    {
+        max = b;
+    }
+if (max < c)
+    {
+    max = c;
+    }
+Console.WriteLine("Число " + max + " является максимальным числом");
+  
